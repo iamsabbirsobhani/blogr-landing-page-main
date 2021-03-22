e462a13399ff4f7502b3f94445ea250f0c57623d
Sabbir Sobhani <sabbirsobhani@hotmail.com>
2021-03-22 23:41:01 +0600

    Section-1 build finished

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -4,10 +4,20 @@
 
       <div class="fit column no-wrap justify-start items-center content-start">
         <div class="section-1">
-          <Navbar/>
-          <!-- <div class="sec-1"> -->
+          <Navbar class="navbar"/>
+          <div class="sec-1">
+            <h1>A modern publishing platform</h1>
+            <p>Grow your audience and build your online brand</p>
+            <div class="button-sec-1 fit row wrap justify-center items-start content-start">
+            <div class="q-pr-md">
+              <q-btn unelevated rounded size="13px" text-color="white" label="Login" />
+            </div>
+            <div class="q-pl-md">
+              <q-btn unelevated rounded size="13px" color="white" text-color="primary" label="Sign Up" />
+            </div>
+          </div>
+          </div>
             <div class="img"></div>
-          <!-- </div> -->
         </div>
         <div class="section-2"></div>
         <div class="section-3"></div>
@@ -40,19 +50,18 @@ export default{
 
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+/* For positioning an image as a background of a certain portion of a webpage,
+  that image should be wrapped by a relative positioned wrapper. Just as bellow:
+*/
   .section-1{
-    /* border:  5px solid black; */
     border-radius: 0px 0px 0px 50px;
-    height: 300px;
+    height: 400px;
     width:100%;
     position: relative;
     background: linear-gradient(90deg, rgba(255, 143, 112, 0.897) 0%, rgba(255, 61, 84, 0.801) 100%);
   }
-  /* .sec-1{
-    position: relative;
-    height: 100%;
-  } */
+
   .img{
     background-image: url("../assets/bg-pattern-intro.svg");
     background-size: cover;
@@ -62,8 +71,17 @@ export default{
     inset: 0px;
     opacity: 1;
     position: absolute;
-    width: 1200px;
+    width: 100%;
     height: 100%;
-    z-index: 1;
+    z-index: -1;
   }
+  .sec-1{
+    margin: 5px auto;
+    text-align: center;
+    color: white;
+    h1{
+      font-size: 40px;
+    }
+  }
+
 </style>
--- src/pages/Navbar.vue
+++ src/pages/Navbar.vue
@@ -1,5 +1,83 @@
 <template>
-<h1></h1>
+<div class="q-pa-lg fit row wrap justify-between items-start content-start">
+    <q-bar class="qbar " >
+        <div>
+            <img class="q-pr-lg" src="../assets/logo.svg" alt="logo">
+        </div>
+      <div class="cursor-pointer non-selectable q-pr-lg" style="color: white;">
+        Product
+        <q-menu >
+          <q-list dense style="min-width: 100px" >
+            <q-item clickable v-close-popup>
+              <q-item-section>Overview</q-item-section>
+            </q-item>
+            <q-item clickable v-close-popup>
+              <q-item-section>Pricing</q-item-section>
+
+            </q-item>
+            <q-item clickable v-close-popup>
+
+              <q-item-section>Marketplace</q-item-section>
+
+            </q-item>
+            <q-item clickable v-close-popup>
+
+              <q-item-section>Features</q-item-section>
+            </q-item>
+            <q-item clickable v-close-popup>
+              <q-item-section>Integrations</q-item-section>
+            </q-item>
+
+          </q-list>
+        </q-menu>
+      </div>
+
+      <div class="q-ml-md cursor-pointer non-selectable q-pr-lg" style="color: white;">
+        Company
+        <q-menu auto-close>
+          <q-list dense style="min-width: 100px">
+            <q-item clickable>
+              <q-item-section>About</q-item-section>
+            </q-item>
+            <q-item clickable>
+              <q-item-section>Blog</q-item-section>
+            </q-item>
+            <q-item clickable>
+              <q-item-section>Careers</q-item-section>
+            </q-item>
+          </q-list>
+        </q-menu>
+      </div>
+
+      <div class="q-ml-md cursor-pointer non-selectable " style="color: white;">
+        Connect
+        <q-menu auto-close>
+          <q-list dense style="min-width: 100px">
+            <q-item clickable>
+              <q-item-section>Contact</q-item-section>
+            </q-item>
+            <q-item clickable>
+              <q-item-section>Newsletter</q-item-section>
+            </q-item>
+            <q-item clickable>
+              <q-item-section>LinkedIn</q-item-section>
+            </q-item>
+            <q-separator />
+          </q-list>
+        </q-menu>
+      </div>
+    </q-bar>
+    <q-bar class="q-pa-md" style="background-color: rgba(0,0,0,0);">
+        <div class="q-pr-md">
+            <q-btn class="login-btn" unelevated rounded size="13px" text-color="white" label="Login" />
+        </div>
+        <div class="q-pl-md">
+            <q-btn class="signup-btn" unelevated rounded size="13px" color="white" text-color="primary" label="Sign Up" />
+        </div>
+    </q-bar>
+
+  </div>
+
 </template>
 
 <script>
@@ -8,6 +86,26 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+h1{
+    font-size: 16px;
+}
+.q-pa-lg{
+    margin: 50px auto;
+    max-width: 1000px;
+    background-color: rgba(54, 14, 14, 0);
+    max-height: 100px;
+    .qbar{
+        background-color: rgba(54, 14, 14, 0);
+    }
+}
 
+.q-btn{
+&:hover{
+  &.signup-btn{
+    background-color: rgba(255, 255, 255, 0.212) !important;
+    color: white !important;
+  }
+}
+}
 </style>
