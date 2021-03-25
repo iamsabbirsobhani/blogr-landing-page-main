182ceb7a6e292c0487c84de84f048a40e93a5c83
Sabbir Sobhani <sabbirsobhani@hotmail.com>
2021-03-26 00:54:42 +0600

    finished section-2

--- src/pages/SectionTwo.vue
+++ src/pages/SectionTwo.vue
@@ -3,15 +3,15 @@
   <div class="main">
       <div class="left">
           <div>
-          <h1> Introducing an extensible editor</h1>
-          <p>  Blogr features an exceedingly intuitive interface which lets you focus on one thing: creating content.
+          <h1>Introducing an extensible editor</h1>
+          <p>Blogr features an exceedingly intuitive interface which lets you focus on one thing: creating content.
   The editor supports management of multiple blogs and allows easy manipulation of embeds such as images,
   videos, and Markdown. Extensibility with plugins and themes provide easy ways to add functionality or
   change the looks of a blog.</p>
           </div>
           <div>
-          <h1>  Robust content management</h1>
-          <p>  Flexible content management enables users to easily move through posts. Increase the usability of your blog
+          <h1>Robust content management</h1>
+          <p>Flexible content management enables users to easily move through posts. Increase the usability of your blog
   by adding customized categories, sections, format, or flow. With this functionality, you’re in full control.</p>
           </div>
       </div>
